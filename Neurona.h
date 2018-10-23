f813491858057d17a160ee0b9875e7df23cb5489
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-10-23 17:50:41 -0500

    Correccion de los argumentos de entrada
    Corregi y agregué lineas ya que me arrojaba errores debido a  los argumentos de entrada. Con esto ya compila pero falta verificar.

--- Neurona.h
+++ Neurona.h
@@ -8,23 +8,27 @@ class Neurona{ //inicio de la clase Neurona
    float x[5];
    float fx, sum;
    int bias=1;
-  
-    
+   
 //metodos
-    void setpesos(float valuep[]){
-	    w[5]=valuep[5]; 
-    }//para meter los pesos en la neurona
-    void setentradas(float valuex[]){
-    		x[5]=valuex[5];
-    }//para meter las entradas a la neurona
+    void setpesos(float valuep){
+	   //para asignar los pesos en la neurona y guardarlos en un arreglo w
+               for(int i=0;i<5;i++){
+               w[i]=valuep;
+               }
+    }
+    void setentradas(float valuex){     //para meter las entradas a la neurona y guardarlas en su arreglo x
+     for(int m=0;m<5;m++){
+        x[m]=valuex;
+     }
+    }
     void sumatoria(){
-	    for(int i=0;i<5;i++){ //sumatoria y activación
-	   sum+=w[i]*x[i];
+	   for(int n=0;n<5;n++){ //sumatoria y activación
+	   sum=sum+w[n]*x[n];
 	}
 	fx=1/(1+exp((sum+bias)*-1));
     }//para realizar la sumatoria y activación
     float getfx(){
 	    return fx;
-    }//para obtener la salida de la neurona	
+    }//para obtener la salida de la neurona
+    	
 };
-
