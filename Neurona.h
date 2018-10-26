356334e6b7ca8d53f84ee6f68c12a77536374268
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-10-25 21:08:52 -0500

    Normalizar entradas
    Normalicé las entradas para ponerlas en un rango de 0 a 1.

--- Neurona.h
+++ Neurona.h
@@ -6,6 +6,7 @@ class Neurona{ //inicio de la clase Neurona
 //atributos
    float w[5];
    float x[5];
+   float normx[5];
    float fx, sum;
    float bias;
    
@@ -26,10 +27,16 @@ class Neurona{ //inicio de la clase Neurona
      for(int m=0;m<5;m++){
         x[m]=valuex;
      }
+	//NORMALIZACION DE ENTRADAS, PARA PONERLAS EN UN RANGO DE 0 A 1
+	normx[0]=(x[0]-0)/(100-0);
+	normx[1]=(x[1]-20)/(70-20);
+	normx[2]=(x[2]-20)/(70-20);
+	normx[3]=(x[3]-0)/(118-0);
+	normx[4]=(x[4]-9)/(180-9);
     }
     void sumatoria(){
 	   for(int n=0;n<5;n++){ //sumatoria y activación
-	   sum=sum+w[n]*x[n];
+	   sum=sum+w[n]*normx[n];
 	}
 	fx=1/(1+exp((sum+bias)*-1));
     }//para realizar la sumatoria y activación
