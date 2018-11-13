958be2c21901872617c7fdf55324fc7a19acca1a
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-11-12 18:27:16 -0600

    Tablas y mas programación
    Agregué varios arrays a la programación junto con la tabla de reglas y algunos algoritmos para relacionar dichos arrays

--- main_practica3.ino
+++ main_practica3.ino
@@ -1,7 +1,115 @@
 
-float gradop[5]; /*array que guarda grados de pertenencias de los 5 conjuntos
+float gradosp[5][5]; /*array que guarda grados de pertenencias de los 5 conjuntos
 difusos de cada una de las 5 variables */
 float entradas[5];
+float min;
+float tablareglas[6][103]= {{0,	0,	0,	0,	1,	2},
+                            {1,	0,	0,	0,	0,	1},
+                            {4,	0,	0,	0,	5,	1},
+                            {5,	0,	0,	0,	0,	1},
+                            {1,	0,	0,	1,	0,	1},
+                            {4,	0,	0,	4,	0,	4},
+                            {3,	0,	0,	5,	0,	5},
+                            {2,	1,	1,	1,	4,	1},
+                            {1,	1,	1,	4,	5,	2},
+                            {3,	1,	1,	4,	5,	4},
+                            {1,	1,	1,	5,	3,	3},
+                            {5,	1,	1,	5,	3,	1},
+                            {4,	1,	2,	1,	1,	1},
+                            {4,	1,	2,	1,	2,	1},
+                            {4,	1,	2,	1,	3,	1},
+                            {5,	1,	2,	2,	3,	2},
+                            {4,	1,	2,	5,	4,	2},
+                            {5,	1,	3,	2,	5,	1},
+                            {4,	1,	3,	4,	4,	2},
+                            {3,	1,	3,	5,	2,	4},
+                            {3,	1,	3,	5,	4,	5},
+                            {5,	1,	3,	5,	3,	2},
+                            {4,	1,	4,	1,	3,	1},
+                            {2,	1,	4,	2,	1,	2},
+                            {5,	1,	4,	3,	2,	1},
+                            {2,	1,	4,	4,	1,	4},
+                            {3,	1,	4,	4,	1,	4},
+                            {3,	1,	4,	5,	3,	4},
+                            {3,	1,	5,	2,	2,	3},
+                            {1,	1,	5,	4,	5,	2},
+                            {2,	1,	5,	4,	2,	5},
+                            {5,	1,	5,	4,	4,	2},
+                            {2,	1,	5,	5,	4,	3},
+                            {3,	2,	1,	2,	4,	3},
+                            {3,	2,	1,	4,	2,	5},
+                            {4,	2,	1,	4,	4,	3},
+                            {4,	2,	2,	2,	3,	3},
+                            {3,	2,	2,	5,	4,	5},
+                            {5,	2,	2,	5,	2,	3},
+                            {5,	2,	2,	5,	5,	2},
+                            {3,	2,	3,	1,	5,	3},
+                            {1,	2,	3,	2,	5,	2},
+                            {1,	2,	3,	4,	1,	1},
+                            {4,	2,	3,	4,	3,	5},
+                            {2,	2,	3,	5,	4,	5},
+                            {3,	2,	3,	5,	3,	5},
+                            {3,	2,	3,	5,	4,	5},
+                            {5,	2,	4,	2,	1,	2},
+                            {3,	2,	4,	4,	3,	4},
+                            {4,	2,	5,	3,	3,	2},
+                            {5,	2,	5,	3,	4,	3},
+                            {2,	2,	5,	5,	3,	3},
+                            {2,	3,	1,	1,	4,	1},
+                            {3,	3,	1,	1,	3,	1},
+                            {3,	3,	1,	3,	3,	4},
+                            {1,	3,	1,	4,	1,	1},
+                            {4,	3,	1,	4,	3,	3},
+                            {4,	3,	2,	1,	3,	1},
+                            {3,	3,	3,	0,	0,	4},
+                            {5,	3,	3,	1,	3,	1},
+                            {2,	3,	3,	2,	3,	2},
+                            {3,	3,	3,	3,	1,	3},
+                            {3,	3,	3,	4,	5,	5},
+                            {2,	3,	3,	5,	5,	5},
+                            {3,	3,	5,	2,	5,	3},
+                            {4,	3,	5,	2,	3,	2},
+                            {3,	3,	5,	3,	5,	3},
+                            {4,	3,	5,	3,	2,	3},
+                            {2,	3,	5,	4,	2,	4},
+                            {2,	3,	5,	4,	5,	3},
+                            {4,	4,	0,	0,	0,	2},
+                            {2,	4,	1,	2,	1,	2},
+                            {1,	4,	1,	4,	1,	3},
+                            {5,	4,	1,	5,	3,	3},
+                            {4,	4,	3,	5,	2,	5},
+                            {4,	4,	3,	5,	4,	1},
+                            {2,	4,	4,	2,	4,	4},
+                            {3,	4,	4,	2,	4,	3},
+                            {5,	4,	4,	3,	3,	2},
+                            {1,	4,	5,	1,	1,	1},
+                            {3,	4,	5,	3,	5,	2},
+                            {4,	4,	5,	3,	3,	4},
+                            {2,	4,	5,	4,	3,	3},
+                            {5,	4,	5,	5,	5,	5},
+                            {5,	5,	0,	0,	0,	1},
+                            {1,	5,	1,	1,	2,	1},
+                            {2,	5,	1,	2,	1,	2},
+                            {3,	5,	1,	2,	1,	3},
+                            {2,	5,	1,	5,	1,	3},
+                            {1,	5,	2,	1,	3,	1},
+                            {2,	5,	2,	1,	3,	3},
+                            {4,	5,	2,	1,	4,	3},
+                            {2,	5,	2,	4,	2,	4},
+                            {2,	5,	3,	2,	4,	3},
+                            {5,	5,	3,	2,	5,	1},
+                            {5,	5,	3,	3,	2,	2},
+                            {4,	5,	4,	1,	1,	1},
+                            {2,	5,	4,	3,	2,	2},
+                            {5,	5,	4,	4,	2,	1},
+                            {4,	5,	5,	1,	4,	1},
+                            {4,	5,	5,	2,	3,	1},
+                            {2,	5,	5,	4,	3,	2},
+                            {4,	5,	5,	5,	2,	4}};
+                     
+float niveles[5]={1,2,3,4,5};
+float conclusiones[5][103];
+float grados_salida[103];
 
 void loop(){
 //las entradas se guardan en un array
@@ -10,13 +118,34 @@ entradas[1]=luz;
 entradas[2]=calidad_aire;
 entradas[3]=pulso;
 entradas[4]=humedad;
-//se sacan los grados de pertenencia finales de cada variable de entrada
-  gradop[0]= grado_temperatura(entrada[0]);
-  gradop[1]= grado_luz_hum_air(entrada[1]);
-  gradop[2]= grado_luz_hum_air(entrada[2]);
-  gradop[3]= grado_pulso(entrada[3]);
-  gradop[4]= grado_luz_hum_air(entrada[4]);
-
+//se sacan los grados de pertenencia de cada variable de entrada
+  gradosp[0]= grado_temperatura(entrada[0]);
+  gradosp[1]= grado_luz_hum_air(entrada[1]);
+  gradosp[2]= grado_luz_hum_air(entrada[2]);
+  gradosp[3]= grado_pulso(entrada[3]);
+  gradosp[4]= grado_luz_hum_air(entrada[4]);
+  
+  //ESTA PARTE DEL CODIGO RELACIONA LOS GRADOS DE PERTENENCIA DE LAS ENTRADAS A LA TABLA DE REGLAS
+  for(int i=0;i<5;i++){
+    for(int j=0;j<5;j++){
+      k=0;
+      while(k<103){
+        if(niveles[j]==tablareglas[j][k]){
+          conclusiones[j][k]=gradosp[i][j];}
+      k++;
+      }
+    }
+  }
+  //ESTA PARTE DEL CODIGO REALIZA LA OPERACION AND EN CADA REGLA Y OBTIENE EL GRADO DE SALIDA
+  for(int m=0;m<5;m++){
+    min=conclusiones[m][0];
+    for(int n=0;n<5;n++){
+        if(conclusiones[m][n] < min){
+          min=conclusiones[m][n];}
+      }
+    grados_salida[m]=min;
+    }
+ 
 }
 float grado_temperatura(float x){ 
 float bajo,muybajo,medio,alto,muyalto;
