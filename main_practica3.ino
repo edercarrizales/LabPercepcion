9b21bd629b8cd639e2942290428fa966faa929a9
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-11-15 10:40:16 -0600

    Codigo del inge
    Cambia totalmente la programación para usar el codigo del inge.

--- main_practica3.ino
+++ main_practica3.ino
@@ -1,155 +1,95 @@
 
 float gradop[5]; /*array que guarda grados de pertenencias de los 5 conjuntos
 difusos de cada una de las 5 variables */
-float entradas[5];
+float inputs[5];
+float nivel_saludable; //salida final del sistema
 
 void loop(){
-//las entradas se guardan en un array
-entradas[0]=temperatura;
-entradas[1]=luz;
-entradas[2]=calidad_aire;
-entradas[3]=pulso;
-entradas[4]=humedad;
-//se sacan los grados de pertenencia finales de cada variable de entrada
-  gradop[0]= grado_temperatura(entrada[0]);
-  gradop[1]= grado_luz_hum_air(entrada[1]);
-  gradop[2]= grado_luz_hum_air(entrada[2]);
-  gradop[3]= grado_pulso(entrada[3]);
-  gradop[4]= grado_luz_hum_air(entrada[4]);
-
-}
-float grado_temperatura(float x){ 
-float bajo,muybajo,medio,alto,muyalto;
-//rutina que obtiene grado de pertenencia para temperatura
-if (x <= 5) { //saca grado en conjunto muy bajo
- muybajo=1;
-}else if (x >= 10) {
- muybajo = 0;
- } else if (x >5 && x <10) {
- muybajo= (10-x)/(10-5);
+ nivel_saludable = FuzzySysT1(inputs,tabla_reglas); /*REVISAR ESTA LINEA*/
+  
+  
 }
 
-if (x <=5 && x>=25) { //saca grado en conjunto bajo
- bajo=0;
-}else if (x > 5 && x <=15) {
- bajo = (x - 5)/(15-5);
- } else if (x >=15 && x <25) {
- bajo= (25-x)/(25-15);
+int reglas=103;
+int entradas = 5;//NTI=NTV-1;
+
+double trapezoidmf(double x,double a,double b,double c,double d){ //FUNCION DE MEMBRESÍA TIPO TRAPEZOIDE
+  double mf=max(min(min((x-a)/(b-a+0.000001),1),(d-x)/(d-c+0.0000001)),0);
+  return mf;
 }
-
-if (x <=15 && x>=35) { //saca grado en conjunto medio
- medio =0;
-}else if (x > 15 && x <=25) {
- medio = (x - 15)/(25-15);
- } else if (x >=25 && x <35) {
- medio = (35-x)/(35-25);
+double trianglemf(double x,double a,double b,double c){ //FUNCION DE MEMBRESIA TIPO TRIANGULAR
+  double mf=max(min((x-a)/(b-a+0.000001),(c-x)/(c-b+0.000001)),0);
+  return mf;
 }
+double Type1FS(double x,int n,double V[]){
+  double a=V[0];
+  double b=V[1];
+  double c=V[2];
+  double mf;
+  if (n== 1){
+      mf=trapezoidmf(x,a-1.0001,a,b,c);
+    return mf;
+    }
+  if (n==2){
+  mf=trianglemf(x,a,b,c);
+  return mf;
+    }
+  if (n==3){
+    mf=trianglemf(x,a,b,c);
+    return mf;
+    }
+    if (n==4){
+    mf=trianglemf(x,a,b,c);
+    return mf;
+    }
+    if (n==5){
+    mf=trapezoidmf(x,a,b,c,c+1);
+    return mf;
+    }
+    if (n==0){
+    mf=1;
+    return mf;
+    }
+  }
+  
+double FuzzySysT1(double X[],int DB[][3])
+{
+    double PARAM[5][3]={{-1.0, -0.6666, -0.3333}
+                      ,{-0.6666, -0.3333, 0}
+                      ,{-0.3333, 0, 0.3333}
+                      ,{0, 0.3333, 0.6666}
+                      ,{0.3333, 0.6666, 1}};
+    double V[3];
+    double AC[5]={-0.5,0,.5,1};
+    double Fo[25];
+    for(int r=0;r<=(reglas-1);r++)
+      {
+      double sumin=1;
+      int n;
+      for(int i=0;i<=(entradas-1);i++)
+          {
+            n=DB[r][i]-1;
+            if(n>-1){
+            V[0]=PARAM[n][0];
+            V[1]=PARAM[n][1];
+            V[2]=PARAM[n][2];
+         }
+          double mf=Type1FS(X[i],(n+1),V);
+          sumin=min(sumin,mf);
+       }
+     Fo[r] = sumin;
+   }
+      double sum1=0; double sum2=0.00000001;
+      for(int r=0;r<=(reglas);r++)
+      {
+      sum1=(sum1+(Fo[r]*AC[DB[r][2]-1]));
+      sum2=(sum2+Fo[r]);
+
+       }
+      double y=sum1/sum2;
+      return y;
+      }
 
 
-if (x <=25 && x>=45) { //saca grado en conjunto alto
- alto =0;
-}else if (x > 25 && x <=35) {
- alto = (x - 25)/(35-25);
- } else if (x >=35 && x <45) {
- alto = (45-x)/(45-35);
-}
 
-if (x <=40) { //saca grado en conjunto muy alto
- muyalto =0;
-}else if (x > 40 && x <45) {
- muyalto = (x-40)/(45-40);
- } else if (x >=45) {
- muyalto = 1;
-}
-
-float grado_luz_hum_air(float x){ 
-float bajo,muybajo,medio,alto,muyalto;
-//rutina que obtiene grado de pertenencia para luz, aire y humedad
-if (x <= 10) { //saca grado en conjunto muy bajo
- muybajo=1;
-}else if (x >= 20) {
- muybajo = 0;
- } else if (x >10 && x <20) {
- muybajo= (20-x)/(20-10);
-}
-
-if (x <=10 && x>=50) { //saca grado en conjunto bajo
- bajo=0;
-}else if (x > 10 && x <=30) {
- bajo = (x - 10)/(30-10);
- } else if (x >=30 && x <50) {
- bajo= (50-x)/(50-30);
-}
-
-if (x <=30 && x>=70) { //saca grado en conjunto medio
- medio =0;
-}else if (x > 30 && x <=50) {
- medio = (x - 30)/(50-30);
- } else if (x >=50 && x <70) {
- medio = (70-x)/(70-50);
-}
-
-
-if (x <=50 && x>=90) { //saca grado en conjunto alto
- alto =0;
-}else if (x > 50 && x <=70) {
- alto = (x - 50)/(70-50);
- } else if (x >=70 && x <90) {
- alto = (70-x)/(90-70);
-}
-
-if (x <=80) { //saca grado en conjunto muy alto
- muyalto =0;
-}else if (x > 80 && x <90) {
- muyalto = (x-80)/(90-80);
- } else if (x >=90) {
- muyalto = 1;
-}
-
-float grado_pulso(float x){ 
-float bajo,muybajo,medio,alto,muyalto;
-//rutina que obtiene grado de pertenencia PULSO CARDIACO
-if (x <= 0) { //saca grado en conjunto muy bajo
- muybajo=1;
-}else if (x >= 30) {
- muybajo = 0;
- } else if (x >15 && x <30) {
- muybajo= (30-x)/(30-15);
-}
-
-if (x <=10 && x>=60) { //saca grado en conjunto bajo
- bajo=0;
-}else if (x > 10 && x <=35) {
- bajo = (x - 10)/(35-10);
- } else if (x >=35 && x <60) {
- bajo= (60-x)/(60-35);
-}
-
-if (x <=40 && x>=90) { //saca grado en conjunto medio
- medio =0;
-}else if (x > 40 && x <=65) {
- medio = (x - 40)/(65-40);
- } else if (x >=65 && x <90) {
- medio = (90-x)/(90-65);
-}
-
-
-if (x <=70 && x>=120) { //saca grado en conjunto alto
- alto =0;
-}else if (x > 70 && x <=96) {
- alto = (x - 70)/(95-70);
- } else if (x >=95 && x <120) {
- alto = (120-x)/(120-95);
-}
-
-if (x <=100) { //saca grado en conjunto muy alto
- muyalto =0;
-}else if (x > 100 && x <115) {
- muyalto = (x-100)/(115-100);
- } else if (x >=130) {
- muyalto = 1;
-}
-
-
-}
 
