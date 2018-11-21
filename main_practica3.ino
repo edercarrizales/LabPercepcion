fc1e313804e6f25f518d60dde749d72ddab069ee
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-11-21 16:28:31 -0600

    correccion de algunos errores
    Se hiciron unas correcciones ya que no compilaba en otras computadoras.

--- main_practica3.ino
+++ main_practica3.ino
@@ -1,6 +1,4 @@
 
-float gradop[5]; /*array que guarda grados de pertenencias de los 5 conjuntos
-difusos de cada una de las 5 variables */
 double inputs[5]={30,15,26,65,32};
 double nivel_saludable; //salida final del sistema
 int reglas=103;
@@ -120,11 +118,11 @@ void loop(){
 
 
 double trapezoidmf(double x,double a,double b,double c,double d){ //FUNCION DE MEMBRESÍA TIPO TRAPEZOIDE
-  double mf=max(min(min((x-a)/(b-a+0.000001),1),(d-x)/(d-c+0.0000001)),0);
+  double mf=max(min(min((x-a)/(b-a+0.000001),1),(d-x)/(d-c+0.0000001)),0.000000);
   return mf;
 }
 double trianglemf(double x,double a,double b,double c){ //FUNCION DE MEMBRESIA TIPO TRIANGULAR
-  double mf=max(min((x-a)/(b-a+0.000001),(c-x)/(c-b+0.000001)),0);
+  double mf=max(min((x-a)/(b-a+0.000001),(c-x)/(c-b+0.000001)),0.000000);
   return mf;
 }
 double Type1FS(double x,int n,double V[]){
@@ -160,11 +158,11 @@ double Type1FS(double x,int n,double V[]){
   
 double FuzzySysT1(double X[],int DB[][6])
 {
-    double PARAM[5][3]={{-1.0, -0.6666, -0.3333}
-                      ,{-0.6666, -0.3333, 0}
-                      ,{-0.3333, 0, 0.3333}
-                      ,{0, 0.3333, 0.6666}
-                      ,{0.3333, 0.6666, 1}};
+    double PARAM[5][3]={{0,0.125 ,0.25 }
+                      ,{0.125, 0.5, 0.625}
+                      ,{0.5, 0.625, 0.75}
+                      ,{0.625, 0.75, 0.875}
+                      ,{0.75, 0.875, 1}};
     double V[3];
     double AC[5]={0,0.25,0.5,0.75,1};
     double Fo[reglas];
@@ -185,7 +183,7 @@ double FuzzySysT1(double X[],int DB[][6])
    }
       double sum1=0; double sum2=0.00000001;
       for(int r=0;r<=(reglas);r++){
-         sum1=(sum1+(Fo[r]*AC[DB[r][2]-1]));
+         sum1=(sum1+(Fo[r]*AC[DB[r][6]-1]));
          sum2=(sum2+Fo[r]);
          }
       double y=sum1/sum2;
